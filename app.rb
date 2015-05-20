1304c1e71fabc2bad0dab3a91170f86c3f35b576
Life is Tech! <k.himeno314@gmail.com>
2015-05-20 13:14:32 +0000

    make it better

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ post '/new' do
   logger.info "名前：#{params[:user_name]}、内容：#{params[:body]}"
   Contribution.create(:name => params[:user_name], :body => params[:body], :img => "")
   
-  unless params[:img].blank?
+  if params[:file]
     image_upload(params[:file])
   end
   
